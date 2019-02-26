60bbdaddc2a8af7afdf6e9e5bf564f2b9026fcea
wzhang14 <wzhang14@lbpearson.ca>
2019-02-26 12:59:37 -0500

    first commit

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+import { TodoService } from '../../services/todo.service';
+import { flattenStyles } from '@angular/platform-browser/src/dom/dom_renderer';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo:Todo){
+    // Remove from UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    // Remove from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo:Todo){
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    });
+  }
+}
